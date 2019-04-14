efd0aafedf9c25352a5d467c3586bc0cc95596e5
nolions <lv2410@gmail.com>
2019-04-14 21:23:54 +0800

    [revise] 以溫度判斷繼電器狀態&根據模式採用不同溫度標的

--- coffebeanlife/coffebeanlife.ino
+++ coffebeanlife/coffebeanlife.ino
@@ -4,24 +4,36 @@
 #include <Wire.h>
 
 //difine pin
+// Led Green
+const int REDLedPin = 2;
+const int GREENLedPin = 3;
+
 // HC-05
 const byte BTpin = 9;
 const int BTRXpin = 7;
 const int BTTXpin = 8;
+
 // Thermocouple MAX675
-const int ThermDO = 4;
+const int ThermDO = 6;
 const int ThermCS = 5;
-const int ThermCLK = 6;
-//  Buzzer
-const int BuzzerPin = 13;
-// Led Green
-const int LedPin = 11;
+const int ThermCLK = 4;
+
 // relay(SRD)
 const int RelayPin = 12;
+
+//  Buzzer
+const int BuzzerPin = 13;
 
 // Led Light Model
 const int LED_MODEL_Start = 0;
 const int LED_MODEL_BTConn = 1;
+// Run Model
+const String RUN_MODEL_MANUAL = "m\r";
+const String RUN_MODEL_AUTO = "a\r";
+// Relay ready
+const String RELAY_READY_OPEN = "o\r";
+const String RELAY_READY_CLOSE = "c\r";
+
 
 MAX6675 thermo(ThermCLK, ThermCS, ThermDO);
 SoftwareSerial BT(BTRXpin,BTTXpin);
@@ -31,9 +43,13 @@ int waitSec = 0;
 double beansTemp = 0;
 double stoveTemp = 0;
 double envTemp = 0;
+double targetTemp = 0;
+double nowTemp = 0;
 
+int runModel;
 int isAlertStatus = 0;
 bool startRecieve = false;
+bool relaySatus = false;
 char val;
 String recieveData = "";
 
@@ -42,22 +58,23 @@ void setup() {
   Serial.begin(9600);
   BT.begin(9600);
   mlx.begin(); 
-  pinMode(LedPin, OUTPUT);
+  pinMode(GREENLedPin, OUTPUT);
   ledLight(LED_MODEL_Start);
   alarmBeep(1, 50);
   delay(500);
 }
 
 void loop() {
-//  Serial.println( isAlertStatus); 
   recieveData = "";
-  waitSec = 1000;
-  
+  waitSec = 2000;
+
+  // 取得溫度
   beansTemp = getKTypeTemp();
   stoveTemp = getObjectTemp();
   envTemp = getAmbientTemp();
 
   if (isBTConn()) {
+    // 藍芽Client裝置連線
     if (isAlertStatus <= 0) {
       isAlertStatus = 1;
     }
@@ -69,7 +86,6 @@ void loop() {
     isAlertStatus++;
     ledLight(LED_MODEL_BTConn);
     
-//    String jsonStr = genTempJSONStr(beansTemp, 0, 0);
     //  透過藍芽回傳溫度
     bluetoothWrite(beansTemp, stoveTemp, envTemp);
     
@@ -84,6 +100,7 @@ void loop() {
       waitSec -= 200;
     }
   } else {
+    // 未有藍芽Client裝置連線
     if (isAlertStatus > 0) {
       isAlertStatus = -1;
     }
@@ -92,18 +109,39 @@ void loop() {
        alarmBeep(2, 50);
     }
     isAlertStatus--;
-    setRelay(0);
+    // 
+    relaySatus = false;
+    RelayLEDStatus(false);
   }
 
   if(startRecieve){
     Serial.println(recieveData);
-    if (recieveData == "1\r") {
-      Serial.println("1");
-      setRelay(1);
-    } else if (recieveData == "0\r") {
-      Serial.println("0");
-      setRelay(0);
+    
+    if( recieveData == RUN_MODEL_MANUAL) {
+      // 根據模式決定目標溫度判斷依據
+      // 手動模式 => 豆溫
+      Serial.println("Model:MANUAL" );
+      nowTemp = beansTemp;
+    } else if( recieveData == RUN_MODEL_AUTO) {
+      // 根據模式決定目標溫度判斷依據
+      // 自動模式 => 爐溫
+      Serial.println("Model:AUTO" );
+      nowTemp = stoveTemp;
+    } else if (recieveData == RELAY_READY_OPEN) {
+      Serial.println("RELAY Ready:YES" );
+      RelayLEDStatus(true);
+      relaySatus = true;
+    } else if (recieveData == RELAY_READY_CLOSE) {
+      Serial.println("RELAY Ready:NO" );
+      RelayLEDStatus(false);
+      relaySatus = false;
+    } else {
+      // 設定目標溫度
+      targetTemp = recieveData.toInt();
     }
+
+    // 設定繼電器狀態
+    setRelay(nowTemp);
     
     startRecieve = false;
     recieveData = "";
@@ -162,33 +200,51 @@ void alarmBeep(int count, int mSec) {
 void ledLight(int model) {
   switch (model){
     case LED_MODEL_Start:
-      LEDStartAction();
+      BLELEDStartAction();
     break;
     case LED_MODEL_BTConn:
-      LEDBTConnAction();
+      BLELEDBTConnAction();
     break;
   }
 }
 
-void setRelay(int relayState) {
-  digitalWrite(RelayPin, relayState);
+void setRelay(double nowTemp) {
+  Serial.print("now:" );
+  Serial.println(nowTemp);
+  Serial.print("target:" );
+  Serial.println(targetTemp);
+  
+  int isStart = HIGH;
+  if (relaySatus && targetTemp > nowTemp) {
+    digitalWrite(RelayPin, HIGH);
+  } else {
+    digitalWrite(RelayPin, LOW);
+  }
 }
 
-void LEDStartAction() {
-  digitalWrite(LedPin, HIGH);
+void RelayLEDStatus(bool status) {
+  if (status) {
+    digitalWrite(REDLedPin, HIGH);
+  } else {
+    digitalWrite(REDLedPin, LOW);
+  }
 }
 
-void LEDBTConnAction() {
-  digitalWrite(LedPin, LOW);
+void BLELEDStartAction() {
+  digitalWrite(GREENLedPin, HIGH);
+}
+
+void BLELEDBTConnAction() {
+  digitalWrite(GREENLedPin, LOW);
   delay(100);
   waitSec -=100;
-  digitalWrite(LedPin, HIGH);
+  digitalWrite(GREENLedPin, HIGH);
   delay(100);
   waitSec -=100;
-  digitalWrite(LedPin, LOW);
+  digitalWrite(GREENLedPin, LOW);
   delay(100);
   waitSec -=100;
-  digitalWrite(LedPin, HIGH);
+  digitalWrite(GREENLedPin, HIGH);
 }
 /**
  * 藍芽連線是否建立
@@ -198,19 +254,4 @@ bool isBTConn() {
     return true;
   }
   return false;
-}
-
-/**
- * 產生相關溫度JSON字串
- */
-String genTempJSONStr(double beans, double stove, double env) {
-  String jsonStr = "{\"b\":";
-  jsonStr += String(beans,2);
-  jsonStr += ",\"s\":";
-  jsonStr += String(stove,2);
-  jsonStr += ",\"e\":";
-  jsonStr += String(env,2);
-  jsonStr += "}";
-
-  return jsonStr;
 }
